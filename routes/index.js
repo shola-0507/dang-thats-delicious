72ad0adc79be2d5e9cd0a289223fa4cbe88245a6
Olushola Karokatose <shola.karo@gmail.com>
2019-02-09 11:34:40 +0100

    You can upload store images

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,21 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStore));
-router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
 router.get('/stores', storeController.getStore);
+router.get('/add', storeController.addStore);
+router.post('/add', 
+    storeController.upload, 
+    catchErrors(storeController.resize), 
+    catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
 router.get('/store/:id/edit', catchErrors(storeController.editStore));
-router.post('/add/:id', catchErrors(storeController.updateStore));
+
 
 module.exports = router;
