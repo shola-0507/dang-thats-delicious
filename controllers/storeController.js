4bb7209a7b7c5ed26f5ed9bd8d0f76aafa65d5cb
Olushola Karokatose <shola.karo@gmail.com>
2019-02-14 15:27:32 +0100

    Users can only eidt stores they create

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -40,6 +40,7 @@ exports.resize = async (req, res, next) => {
 }
 
 exports.createStore = async (req, res) => {
+    req.body.author = req.user._id;
     const store = await new Store(req.body).save();
     req.flash('success', `${store.name} Store created successfully. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
@@ -50,8 +51,15 @@ exports.getStore = async (req, res) => {
     res.render('index', {title: 'Stores', stores});
 }
 
+const confirmOwner = (store, user, callback) => {
+    if (!store.author.equals(user._id)) {
+        callback.redirect('/stores');
+    }
+}
+
 exports.editStore = async (req, res) => {
     const store = await Store.findOne({_id: req.params.id});
+    confirmOwner(store, req.user, res);
     res.render('editStore', {title: `Edit ${store.name}`, store});
 }
 
@@ -69,7 +77,8 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.getStoreBySlug = async (req, res, next) => {
-    const store = await Store.findOne({slug: req.params.slug});
+    const store = await Store.findOne({slug: req.params.slug})
+                            .populate('author');
     if (!store) {
         return next();
     }
